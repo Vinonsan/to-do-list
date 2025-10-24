5a32a90c4bbfea220d24afc84fcbb8b3c84163b3
Rebuild52 <support@rebuild.lk>
2025-10-24 10:46:38 +0100

    task done

--- todo/src/Application.jsx
+++ todo/src/Application.jsx
@@ -27,23 +27,20 @@ function Application() {
 
     return (
         <div className='flex flex-col gap-3'>
-            <div className='w-full h-56 bg-red-800 p-2 flex justify-between items-center'>
-                <h1>This is To do app</h1>
-                <div className='flex flex-col '>
-                    <h2>number of task : {items.length}</h2>
-                    <h3>done task : 3</h3>
-                </div>
+            <div className='w-full h-auto bg-red-800 p-2 flex justify-between items-center text-xl font-bold'>
+                 <h2>Number of task : {items.length}</h2>
+                 <h3>Completed task : 3</h3>
             </div>
             <div className='flex gap-4'>
-                <input value={value} onChange={handleChange} className='w-full h-16 border-2 border-red-50 text-red-50 font-bold text-base' />
-                <button onClick={handleClick} className='bg-amber-400 w-16 h-16'>+</button>
+                <input value={value} onChange={handleChange} placeholder="Enter task"  className='w-full h-12 border-2 border-red-50 text-red-50 font-bold text-base placeholder:text-white/30 pl-2' />
+                <button onClick={handleClick}   className='bg-red-700 text-red-50 text-4xl w-12 h-12 flex items-center justify-center'>+</button>
             </div>
             <ul className='flex flex-col gap-5'>
                 {items.map(item =>
                     <li key={item.id} className='text-2xl text-amber-50'>
-                        <div className='flex justify-between gap-2' >
-                            <div className='bg-green-500 p-2 w-full text-base text-amber-50 flex items-center justify-start px-4'>{item.task}</div>
-                            <button onClick={() => { }} className='rounded-full h-16 w-16 bg-yellow-400 flex items-center justify-center'></button>
+                        <div className='flex justify-between gap-4' >
+                            <div className='bg-red-50 p-2 w-full text-2xl font-bold text-black flex items-center justify-start px-4'>{item.task}</div>
+                            <button onClick={() => { }} className='rounded-full h-12 w-12 bg-white text-xs text-black flex items-center justify-center'>done</button>
                         </div>
                     </li>
                 )}
